4609045bacb7f9e803b489e30c1bc760701cf941
Summer Rizzo <srainrizzo@gmail.com>
2020-01-17 11:11:49 -0500

    donezo

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,21 @@
 class GuestsController < ApplicationController
 
+  #Read
   def index
     @guests = Guest.all
+
+    render :index
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+
+    #Sort episodes by highest rating
+    @sorted_episodes = @guest.sort_episodes_by_rating
+    
+    render :show
+  end
+
+  
+
 end
