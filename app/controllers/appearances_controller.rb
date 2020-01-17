f093a79472e1c7800914a2faaed869d6830d7a50
github username <github email address>
2020-01-17 11:10:57 -0500

    done

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,27 @@
+class AppearancesController < ApplicationController
+
+  def new
+    @appearance = Appearance.new
+    @guests = Guest.all 
+    @episodes = Episode.all
+  end
+
+  def create
+    @appearance = Appearance.create(appearance_params)
+    @episode = Episode.find(params[:appearance][:episode_id])
+
+    if @appearance.valid?
+      redirect_to episode_path(@episode)
+    else
+      flash[:errors] = @appearance.errors.full_messages
+      redirect_to new_appearance_path
+    end
+  end
+
+    private
+
+  def appearance_params
+    params.require(:appearance).permit(:rating, :guest_id, :episode_id)
+  end
+  
+end
