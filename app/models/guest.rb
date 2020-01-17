4a25e974f54ed7e1fca30276b3d340f08d0c6926
teapuddles <kingsley.gleeson@gmail.com>
2020-01-17 11:11:17 -0500

    done

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,15 @@
 class Guest < ApplicationRecord
+  has_many :appearances
+  has_many :episodes, through: :appearances
+
+
+  def order_appearance
+    self.appearances.sort_by do |appearance|
+      byebug
+      appearance[:rating]
+    end
+  end
+
+
 
 end
